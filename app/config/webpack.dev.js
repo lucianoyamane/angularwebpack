c327f6831c70d5dea19c3a2b11a3a3b552c35dba
lyamanek <luciano.oliveira@klab.com.br>
2022-01-22 09:42:15 -0300

    change output folder

--- app/config/webpack.dev.js
+++ app/config/webpack.dev.js
@@ -45,7 +45,7 @@ module.exports = function () {
        *
        * See: https://webpack.js.org/configuration/output/#output-path
        */
-      path: helpers.root('dist'),
+      path: helpers.root('../docs'),
 
       /**
        * Specifies the name of each output file on disk.
