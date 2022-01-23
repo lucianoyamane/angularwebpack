40e395c541e0f235b4c6e81c9f423ad2d338fd36
lyamanek <luciano.oliveira@klab.com.br>
2022-01-23 10:29:39 -0300

    update angular to 9

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,7 +4,15 @@ import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-import { MatSidenavModule, MatIconModule, MatGridListModule, MatToolbarModule, MatMenuModule, MatStepperModule, MatTabsModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatInputModule } from '@angular/material/input';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { ROUTES } from './app.routes';
